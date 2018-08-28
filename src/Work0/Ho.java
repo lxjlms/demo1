e20507338307ee0bb46550986cf86116698c670c
lxjlms <m15232129265@163.com>
2018-08-28 11:39:04 +0800

    改

--- src/Work0/Ho.java
+++ src/Work0/Ho.java
@@ -3,6 +3,7 @@ package Work0;
 public class Ho {
 
 	public static void main(String[] args) {
+		System.out.println("what is matter with you?  oh! please do not leave me!!!");
 		System.out.println("what time is it?");
 		System.out.println("Hello Word");
 
